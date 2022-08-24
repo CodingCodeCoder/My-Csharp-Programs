7ff250bd3d0078c97dae6b39e9b66912e31f6a37
Simon Dionne-Couture <simon.dionne.couture@hotmail.com>
2022-08-24 16:24:21 -0400

    commit of my C# programs

---
+++ Exception Handling/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+    Exception Handling
+    Simon Dionne-Couture
+
+    This program handles Exceptions.
+ */
+using System;
+
+namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//Start of MAIN
+            //Variable Initialization
+            string inputA;
+            string inputB;
+            byte a;
+            byte b;
+
+            //User Input
+            Console.WriteLine("Enter a number between 0 and 255:");
+            inputA = Console.ReadLine();
+            Console.WriteLine("Enter another number between 0 and 255:"); 
+            inputB = Console.ReadLine();
+
+            //Exception Handling
+            try
+            {
+                a = Byte.Parse(inputA);
+                b = Byte.Parse(inputB);
+                Console.WriteLine($"{a} divided by {b} is: {a / b}");
+            } //follow by catch block
+            catch ( FormatException e ) { Console.WriteLine(e.Message); }
+            catch ( OverflowException e ) { Console.WriteLine(e.Message); }
+            catch ( DivideByZeroException e) { Console.WriteLine(e.Message); }
+            catch ( Exception e ){ Console.WriteLine(e.Message); }
+        }//END of MAIN
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+    Assignment #1
+    FizzBuzz
+    Simon Dionne-Couture
+    
+    This program counts from 1 to 100 and replace every multiple of 3 and/or 5 with "Fizz", "Buzz" or "FizzBuzz".
+ */
+
+using System;
+namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 100; i++)
+            { //this loop counts to 100
+                if (i % 3 == 0 && i % 5 == 0)
+                {//checks for multiples of 3 and 5 and display "FizzBuzz"
+                    Console.WriteLine("Fizzbuzz");
+                }
+                else if (i % 3 == 0)
+                {//checks for multiples of 3 and display "Fizz"
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {//checks for multiples of 5 and display "Buzz"
+                    Console.WriteLine("Buzz");
+                }
+                else
+                { //displays the count
+                    Console.WriteLine(i);
+                }
+            }
+        } // END OF MAIN
+    }
+}
